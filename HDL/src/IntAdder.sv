e13e0276e948766d79ee0873998f49280470670d
Christian Shakkour <christians@hl-gw101.qb.hailotech>
2023-03-06 23:06:29 +0200

    adds other design modules

---
+++ HDL/src/IntAdder.sv
@@ -0,0 +1,42 @@
+/*
+
+  
+ */
+
+`timescale 1ns/1ns
+
+module IntAdder
+  #(
+    parameter integer  W_IN_A =8,
+    parameter integer  W_IN_B =16,
+    localparam integer W_MAX_OP = ((W_IN_A > W_IN_B) ? W_IN_A : W_IN_B),
+    localparam integer W_OUT_X = W_MAX_OP+1;
+    )
+  (
+   input logic [W_IN_A-1:0]   in_a, //signed
+   input logic [W_IN_B-1:0]   in_b, //signed
+   output logic [W_OUT_X-1:0] out_x
+   );
+  
+  logic                       sign_of_a;
+  logic [W_MAX_OP-1:0]        a_extended;
+  logic [W_MAX_OP-1:0]        a_pre_adder;
+
+  logic                       sign_of_b;
+  logic [W_MAX_OP-1:0]        b_extended;
+  logic [W_MAX_OP-1:0]        b_pre_adder;
+
+  // operand a sign extension and handeling
+  assign sign_of_a = in_a[W_IN_A-1];
+  assign a_extended = (W_MAX_OP==W_IN_A) ? in_a : signed'(in_a);
+  assign a_pre_adder = (sign_of_a ? -a_extended : a_extended);
+  
+  // operand b sign extension and handeling
+  assign sign_of_b = in_b[W_IN_B-1];  
+  assign b_extended = (W_MAX_OP==W_IN_B) ? in_b : signed'(in_b);
+  assign b_pre_adder = (sign_of_b ? -b_extended : b_extended);
+
+  // result
+  out_x = a_pre_adder + b_pre_adder;
+  
+endmodule // IntAdder
--- HDL/synth/IntMultVariations.sv
+++ HDL/synth/IntMultVariations.sv
@@ -1,67 +1,67 @@
 
 `timescale 1ns/1ns
 
-module FpMultVariations
+module IntMultVariations
   #(
     // W_FP_THRESHHOLD = W_MANTISSA + W_EXPONENT + 1
-    localparam W_INT  = 32,
-    localparam W_JUMP = 8, 
-    // mantissa min/max boundries
-    localparam OP_A_MAX = W_INT/W_JUMP,
-    localparam OP_A_MIN = 1,
-    // exponent min/max boundries
-    localparam OP_B_MAX = W_INT/W_JUMP,
-    localparam OP_B_MIN = 1,
-    localparam W_OP_OUT = 2*W_INT; 
+    localparam W_INT  = 16,
+    localparam W_JUMP = 1, 
+    // op A min/max boundries
+    localparam OP_A_MAX = W_INT,
+    localparam OP_A_MIN = 8,
+    // op B min/max boundries
+    localparam OP_B_MAX = W_INT,
+    localparam OP_B_MIN = 8,
+    localparam W_OP_OUT = 2*W_INT 
     )
-   (
-    input logic 	     clk,
-    input logic 	     rstn,
-    input logic [W_INT-1:0]  in_a [OP_B_MAX:OP_B_MIN][OP_A_MAX:OP_A_MIN],
-    input logic [W_INT-1:0]  in_b [OP_B_MAX:OP_B_MIN][OP_A_MAX:OP_A_MIN],
-    output logic [W_OP_OUT-1:0] out_x_reg [OP_B_MAX:OP_B_MIN][OP_A_MAX:OP_A_MIN]
-    );
-   
-   genvar 			      op_a_i;
-   genvar 			      op_b_i;
+  (
+   input logic                 clk,
+   input logic                 rstn,
+   input logic [W_INT-1:0]     in_a [OP_B_MAX:OP_B_MIN][OP_A_MAX:OP_A_MIN],
+   input logic [W_INT-1:0]     in_b [OP_B_MAX:OP_B_MIN][OP_A_MAX:OP_A_MIN],
+   output logic [W_OP_OUT-1:0] out_x_reg [OP_B_MAX:OP_B_MIN][OP_A_MAX:OP_A_MIN]
+   );
+  
+  genvar                       op_a_i;
+  //genvar                       op_b_i;
 
-   logic [W_INT-1:0] 		      in_a_reg  [OP_B_MAX:OP_B_MIN][OP_A_MAX:OP_A_MIN];
-   logic [W_INT-1:0] 		      in_b_reg  [OP_B_MAX:OP_B_MIN][OP_A_MAX:OP_A_MIN];
-   logic [W_OP_OUT-1:0] 	      out_x     [OP_B_MAX:OP_B_MIN][OP_A_MAX:OP_A_MIN];
-   
-   generate
-      for(op_a_i=OP_B_MIN; op_a_i<=OP_B_MAX; op_a_i++) begin : operand_b
-	 for(op_b_i=OP_A_MIN; op_b_i<=OP_A_MAX; op_b_i++) begin : operand_a
+  logic [W_INT-1:0]            in_a_reg  [OP_B_MAX:OP_B_MIN][OP_A_MAX:OP_A_MIN];
+  logic [W_INT-1:0]            in_b_reg  [OP_B_MAX:OP_B_MIN][OP_A_MAX:OP_A_MIN];
+  logic [W_OP_OUT-1:0]         out_x     [OP_B_MAX:OP_B_MIN][OP_A_MAX:OP_A_MIN];
+  
+  generate
+    for(op_a_i=OP_A_MIN; op_a_i<=OP_A_MAX; op_a_i++) begin : operand_a
+      //for(op_b_i=OP_A_MIN; op_b_i<=OP_A_MAX; op_b_i++) begin : operand_a
 
-	    localparam W_IN_A  = op_a_i * W_JUMP;
-	    localparam W_IN_B  = op_b_i * W_JUMP;
-	    localparam W_OUT_X = W_IN_B + W_IN_A;  
+      localparam W_IN_A  = op_a_i;
+      localparam W_IN_B  = op_a_i;
+      localparam W_OUT_X = W_IN_B + W_IN_A;  
 
-	    
-	    IntMult #(.W_IN_A(W_IN_A), .W_IN_B(W_IN_B))
-	    IntMult_inst
-	      (
-	       .in_a	(in_a_reg  [op_a_i][op_b_i]),
-	       .in_b	(in_b_reg  [op_a_i][op_b_i]),
-	       .out_x	(out_x     [op_a_i][op_b_i])
-	       );
+      
+      IntMult #(.W_IN_A(W_IN_A), .W_IN_B(W_IN_B))
+      IntMult_inst
+        (
+         .in_a    (in_a_reg  [op_a_i][op_a_i]),
+         .in_b    (in_b_reg  [op_a_i][op_a_i]),
+         .out_x   (out_x     [op_a_i][op_a_i])
+         );
 
-	    always_ff @(posedge clk or negedge rstn) begin
-	       if(~rstn) in_a_reg[op_a_i][op_b_i] <= '0;
-	       else      in_a_reg[op_a_i][op_b_i] <= in_a[op_a_i][op_b_i];
-	    end
-
-	    always_ff @(posedge clk or negedge rstn) begin
-	       if(~rstn) in_b_reg[op_a_i][op_b_i] <= '0;
-	       else      in_b_reg[op_a_i][op_b_i] <= in_b[op_a_i][op_b_i];
-	    end
-
-	    always_ff @(posedge clk or negedge rstn) begin
-	       if(~rstn) out_x_reg[op_a_i][op_b_i] <= '0;
-	       else      out_x_reg[op_a_i][op_b_i] <= out_x[op_a_i][op_b_i];
-	    end
-	 end
+      always_ff @(posedge clk or negedge rstn) begin
+        if(~rstn) in_a_reg[op_a_i][op_a_i] <= '0;
+        else      in_a_reg[op_a_i][op_a_i] <= in_a[op_a_i][op_a_i];
       end
-   endgenerate
-   
+
+      always_ff @(posedge clk or negedge rstn) begin
+        if(~rstn) in_b_reg[op_a_i][op_a_i] <= '0;
+        else      in_b_reg[op_a_i][op_a_i] <= in_b[op_a_i][op_a_i];
+      end
+
+      always_ff @(posedge clk or negedge rstn) begin
+        if(~rstn) out_x_reg[op_a_i][op_a_i] <= '0;
+        else      out_x_reg[op_a_i][op_a_i] <= out_x[op_a_i][op_a_i];
+      end
+    end
+    //  end
+  endgenerate
+  
 endmodule // FpMultVariations
